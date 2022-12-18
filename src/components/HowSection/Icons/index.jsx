9cdcaf4f10eed7968caf49b3459379d7211368b9
Zehra <aydin.fzehra@gmail.com>
2022-12-18 21:07:11 +0300

    complete how section

--- src/components/HowSection/Icons/index.jsx
+++ src/components/HowSection/Icons/index.jsx
@@ -1,18 +1,31 @@
 import React from "react";
 import { BsLinkedin, BsTwitter, BsGithub } from "react-icons/bs";
-import { Icon, HStack, Link } from "@chakra-ui/react";
+import { Icon, HStack, Link, ScaleFade } from "@chakra-ui/react";
 
 export function Icons() {
   return (
     <HStack color="gray.600">
       <Link href="https://www.linkedin.com/in/fatma-zehra-aydin/" isExternal>
-        <Icon as={BsLinkedin} boxSize={8} />
+        <Icon
+          as={BsLinkedin}
+          boxSize={8}
+          _hover={{ transform: "scale(1.1)", color: "gray.700" }}
+        />
       </Link>
       <Link href="https://twitter.com/aydinfzehra" isExternal>
-        <Icon as={BsTwitter} boxSize={8} mx={2} />
+        <Icon
+          as={BsTwitter}
+          boxSize={8}
+          mx={2}
+          _hover={{ transform: "scale(1.1)", color: "gray.700" }}
+        />
       </Link>
       <Link href="https://github.com/aydinfz" isExternal>
-        <Icon as={BsGithub} boxSize={8} />
+        <Icon
+          as={BsGithub}
+          boxSize={8}
+          _hover={{ transform: "scale(1.1)", color: "gray.700" }}
+        />
       </Link>
     </HStack>
   );
--- src/components/HowSection/index.jsx
+++ src/components/HowSection/index.jsx
@@ -1,21 +1,19 @@
 import React from "react";
 import { Icons } from "./Icons";
-import { Text, VStack } from "@chakra-ui/react";
+import { Text, VStack, UnorderedList, ListItem } from "@chakra-ui/react";
 
 export function HowSection() {
   return (
-    <VStack h="80vh" textAlign="center" justify="center">
-      <Text mb={10}>
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Beatae facere
-        voluptates dicta iure architecto vitae reiciendis libero labore, modi
-        molestiae temporibus delectus ut commodi quisquam consequatur. Quae
-        repellendus sed est! Lorem ipsum dolor, sit amet consectetur adipisicing
-        elit. Est, quidem. Hic quia ipsam voluptatibus delectus quidem iste
-        repellat rem commodi, illo dicta temporibus ut eaque deleniti optio
-        tempora modi. Doloribus. Lorem ipsum dolor sit amet consectetur
-        adipisicing elit. Nemo sapiente soluta sint aspernatur possimus,
-        pariatur totam illo, perspiciatis quam exercitationem deleniti quibusdam
-        eligendi ipsam repellendus tempore expedita placeat praesentium odio.
+    <VStack h="85vh"  justify="space-between">
+      <Text mt={32}>
+        <UnorderedList spacing={4} fontSize="17px">
+          <ListItem>You can add a reservation by using input section.</ListItem>
+          <ListItem>Click on a reservation card to change it status.</ListItem>
+          <ListItem>Hover on a card to see delete button.</ListItem>
+          <ListItem>Confirm a reservation by double click on it.</ListItem>
+          <ListItem>Check which services asked for a confirmed reservation.</ListItem>
+          <ListItem>Use time picker to select a date on right upper side.</ListItem>
+        </UnorderedList>
       </Text>
       <Icons />
     </VStack>
