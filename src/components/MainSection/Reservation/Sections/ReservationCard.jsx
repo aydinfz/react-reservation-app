070c36f9114fe5d51c89d6ad05cdc539b77f7113
Zehra <aydin.fzehra@gmail.com>
2022-12-18 15:33:07 +0300

    complete reservation funct and style

--- src/components/MainSection/Reservation/Sections/ReservationCard.jsx
+++ src/components/MainSection/Reservation/Sections/ReservationCard.jsx
@@ -1,14 +1,13 @@
-import React, { useState } from "react";
-import { Box, Badge } from "@chakra-ui/react";
+import React, { useState, useRef } from "react";
+import { Box, Badge, Icon } from "@chakra-ui/react";
+import { FaTrashAlt } from "react-icons/fa";
 import { useDispatch } from "react-redux";
 import { removeReservation } from "../../../../redux/reservationSlice";
 import { addConfirmed } from "../../../../redux/confirmedSlice";
 import { nanoid } from "@reduxjs/toolkit";
 
-
 export function ReservationCard({ name, status, id }) {
   const [state, setState] = useState(status);
-
   const dispatch = useDispatch();
 
   const handleChangeStatus = () => {
@@ -19,32 +18,49 @@ export function ReservationCard({ name, status, id }) {
     }
   };
 
-  const handleConfirmation = () => {
+  const handleRemove = () => {
     dispatch(removeReservation(id));
-    dispatch(addConfirmed({
-      id: nanoid(),
-      name,
-      services: []
-    }))
+  };
+
+  const handleConfirmation = () => {
+    handleRemove();
+    dispatch(
+      addConfirmed({
+        id: nanoid(),
+        name,
+        services: [],
+      })
+    );
   };
 
   return (
-    <Box
-      borderColor="purple.400"
-      borderRadius="md"
-      borderStyle="solid"
-      borderWidth="2px"
-      w="95%"
-      m={2}
-      p={2}
-      color="black"
-      onClick={handleChangeStatus}
-      onDoubleClick={handleConfirmation}
-    >
-      {name}
-      <Badge colorScheme={state == "cancel" ? "blackAlpha" : "purple"} mx={2}>
-        {state}
-      </Badge>
+    <Box display="flex" alignItems="center" role="group">
+      <Box
+        borderColor="purple.400"
+        borderRadius="md"
+        borderStyle="solid"
+        borderWidth="2px"
+        w="90%"
+        m={2}
+        p={2}
+        color="black"
+        _hover={{ bg: "gray.100" }}
+        onClick={handleChangeStatus}
+        onDoubleClick={handleConfirmation}
+      >
+        {name}
+        <Badge colorScheme={state == "cancel" ? "blackAlpha" : "purple"} mx={2}>
+          {state}
+        </Badge>
+      </Box>
+      <Icon
+        as={FaTrashAlt}
+        boxSize="18px"
+        color="gray"
+        visibility="hidden"
+        _groupHover={{ visibility: "visible" }}
+        onClick={handleRemove}
+      />
     </Box>
   );
 }
